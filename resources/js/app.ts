ab5e04ebcfba31420fb13157bd1708b56048e0b2
Farhad Zaman <fzpolok@gmail.com>
2025-01-26 18:26:36 +0600

    bug fix

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,8 +4,7 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, DefineComponent, h } from 'vue';
-import { ZiggyVue } from 'ziggy-js';
-import VaporMixin from "@/Mixins/VaporMixin";
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -20,7 +19,6 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .mixin(VaporMixin)
             .mount(el);
     },
     progress: {
